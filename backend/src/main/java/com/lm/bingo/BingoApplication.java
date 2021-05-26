b697f2818b5182f25f727aeff4699b5379633fc1
lucas <lucas-mira1@hotmail.com>
2021-05-26 12:03:48 -0300

    Backend created

---
+++ backend/src/main/java/com/lm/bingo/BingoApplication.java
@@ -0,0 +1,13 @@
+package com.lm.bingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BingoApplication.class, args);
+	}
+
+}
